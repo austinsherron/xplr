5c063a078328c680525a3e3345b945bcfe8298f0
Austin Sherron <austin@pryvate.email>
2024-02-11 04:19:56 -0800

    [WIP] Update Logger
    * Update logger to conform w/ other projects and apis for shared utils

--- lua/xplr/utils/globals.lua
+++ lua/xplr/utils/globals.lua
@@ -1,6 +1,10 @@
 ---@note: we want global functions w/ lowercased names
 ---@diagnostic disable: lowercase-global
 
+-- xplr config object
+XplrConfig = require('toolbox.app.config').new 'xplr'
+Logger = require('toolbox.app.logger').new(Config, require('toolbox.log.type').XPLR)
+
 -- generally useful, oft imported utils
 Array = require 'toolbox.core.array'
 Bool = require 'toolbox.core.bool'
@@ -12,6 +16,8 @@ Err = require 'toolbox.error.error'
 Set = require 'toolbox.extensions.set'
 Lazy = require 'toolbox.utils.lazy'
 Map = require 'toolbox.utils.map'
+
+-- error handling utils
 OnErr = require 'utils.error.onerr'
 Safe = require 'utils.error.safe'
 
@@ -21,7 +27,3 @@ filter = Map.filter
 foreach = Map.foreach
 map = Map.map
 fmt = String.fmt
-
--- xplr specific config
-Config = require('toolbox.app.config').new 'xplr'
-Logger = require('toolbox.app.logger').new(Config, require('toolbox.log.type').XPLR)
---
+++ lua/xplr/utils/logger.lua
@@ -0,0 +1,37 @@
+local Lazy = require 'toolbox.utils.lazy'
+
+local LogLevel = require 'toolbox.log.level'
+local Logger = require 'toolbox.log.logger'
+local LoggerType = require 'toolbox.log.type'
+
+local level = XplrConfig.log_level() or LogLevel.default()
+
+local logger = Logger.new(LoggerType.XPLR, level)
+
+local LOGGERS = {
+  INIT = Lazy.value(function()
+    return logger:sub 'INIT'
+  end),
+  SPOONS = Lazy.value(function()
+    return logger:sub 'XPM'
+  end),
+}
+
+--- Gets the root logger instance or the scoped sub-logger instance for the provided
+--- label.
+---
+---@param label string|nil: optional; the sub-logger to get
+---@return Logger: the root logger instance, if label is nil, or the sub-logger instance
+--- for the provided label
+---@error if the provided label doesn't correspond to a known sub-logger
+return function(label)
+  if label == nil then
+    return logger
+  end
+
+  if LOGGERS[label] == nil then
+    error(string.format('No sub-logger=%s', label))
+  end
+
+  return LOGGERS[label] --[[@as Logger]]
+end
